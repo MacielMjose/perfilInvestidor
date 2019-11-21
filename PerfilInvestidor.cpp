fc04a29d6da7f3717f3f52e100367a62ef75d416
rennerdb <rennerdb@DBS-NOTE-228.dbserver.com.br>
2019-11-21 01:43:19 -0300

    subida programa com correção, todas funcoes ok

--- PerfilInvestidor.cpp
+++ PerfilInvestidor.cpp
@@ -1,3 +1,4 @@
+  
 /***********************************************/ 
 /* Árvores Binárias                            */
 /* objetivo: registro de códigos               */
@@ -11,7 +12,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>    // setlocale
-#include <ctype.h>//biblioteca p/ as funções isalpha isdigit toupper tolower
+
 
 #define FALSE   0      // constante falsa
 #define TRUE   !FALSE  // constante verdadeira
@@ -24,7 +25,7 @@ typedef struct {          // registro
        int  codigo;       
 	   int  altura;
 	   int  fatbal;
-	   char pergunta[]; //mudar nome dessa variável depois
+	   char nome[500]; //mudar nome dessa variável depois
 } INFORMACAO;
        
 typedef struct arv {
@@ -33,15 +34,25 @@ typedef struct arv {
        struct arv* sube;  // ponteiro para o nodo da esquerda
 } ARVORE; 
 
+
+//#include "GDPerfilInvestidor.h"
+
+
 /***********************************************/ 
 /* Definição das Funções                       */
 /***********************************************/
 void    cria_ARVORE     ( ARVORE** r );  // inicializa árvore com NULL
-void    imprime_ARVORE  ( ARVORE* aux ); // visualização da árvore em tela, todos os registros
-ARVORE* remove_recursivo( ARVORE *p, int cod );
-ARVORE* inicia_questionario( ARVORE *p); //rotina responsavel por realizar as perguntas a fim de traçar o perfil do investidor
+void    entrada_dados   ( ARVORE* aux ); // leitura dos dados de entrada
 void gera_dados(ARVORE **r); 
- 
+void    imprime_ARVORE  ( ARVORE* aux ); // visualização da árvore em tela, todos os registros
+void    insere          ( ARVORE** r );  // inclui um novo registro na árvore, sempre na folha
+void    insere_recursivo( ARVORE** r, int cod ); // inclui um novo registro na árvore
+ARVORE* busca_recursivo ( ARVORE *p, int cod ); 
+ARVORE* inicia_questionario( ARVORE *p); //rotina responsavel por realizar as perguntas a fim de traçar o perfil do investidor
+int     busca           ( int matricula, ARVORE** a, ARVORE** p ); // procura na árvore um código
+ARVORE* remove_recursivo( ARVORE *p, int cod );
+
+       
 /***********************************************/ 
 /* Programa Principal                          */
 /***********************************************/
@@ -55,12 +66,12 @@ int main( void )
     cria_ARVORE( &r );
     while( 1 ){
          printf( "\n /---------------------------------------------------/" ); 
-         printf( "\n Perfil de Investidor  - Menu                         " );
+         printf( "\n Programa de registros - Menu                         " );
          printf( "\n [1] Cria ARVORE                                      " );
          printf( "\n [4] Imprime                                          " );         
 		 printf( "\n [9] Gera Arvore Binária - Popular Árvore             " ); 
-		 printf( "\n [5] Inicia Questionário                              " );         
-		 printf( "\n [7] Para sair do programa                            " );         
+		 printf( "\n [5] Inicia Questionário                             " );         
+		 printf( "\n [7] Para sair do programa                           " );         
          printf( "\n /---------------------------------------------------/" );      
          printf( "\n Opcao: " );
          op = getche(); // tecla de opção do menu
@@ -69,10 +80,22 @@ int main( void )
            case '1':   // rotina cria ARVORE       
                    cria_ARVORE( &r );
                    break;
+                                
+           case '2':   // rotina inclui nodo no final da ARVORE (folha)
+                   insere( &r );    
+                   break;
            
            case '4':   // rotina imprime nodos da ARVORE
                    imprime_ARVORE( r );
                    break;                   
+                   
+           case '6':  // rotina recursiva que busca um registro da ARVORE                                                
+                   printf("\n Buscar o codigo: ");
+                   scanf("%d", &cod);
+                   p= busca_recursivo( r, cod );
+                   if( p != NULL )
+                       printf("\n Codigo: %d", p->info.codigo);   
+                   break; 
                    
            case '8':  // rotina recursiva que busca um registro da ARVORE                                                
                    //p= menor_valor( r );
@@ -80,7 +103,7 @@ int main( void )
                        printf("\n Codigo: %d", p->info.codigo);
                    break;
                    
-            case '9': // rotina gera dados de forma automática para povoar a Arvore
+           case '9': // rotina gera dados de forma automática para povoar a Arvore
            
                     gera_dados(&r);
                     break;
@@ -113,9 +136,9 @@ int main( void )
 
 /***************************************************
  * iniciar questionario                            *
- * objetivo: determinar caminho a seguir           *
- * entrada : ARVORE *p                             *
- * saída   : perfil de investidor                  *
+ * objetivo: rotina para buscar registro por código*
+ * entrada : ARVORE e cod                          *
+ * saída   : ARVORE com mais um registro           *
  ***************************************************/ 
 ARVORE* inicia_questionario( ARVORE *p)
 {
@@ -123,7 +146,7 @@ ARVORE* inicia_questionario( ARVORE *p)
     	
     	do
 		{
-			printf("\n %s",p->info.pergunta); //inicializa o qestioranio com a pergunta raiz
+			printf("\n %s",p->info.nome); //inicializa o qestioranio com a pergunta raiz
 	   		printf("\n Resposta: "); //pergunta ao usuário
 	   		gets(resposta); //get string da resposta do usuário
 	   		strupr(resposta);//string uppercase, transforma o string digitado em Uppercase
@@ -138,9 +161,21 @@ ARVORE* inicia_questionario( ARVORE *p)
 	   			printf("\n Resposta inválida");
 				   	   		
 	   		return 0;//se o usuário digitar qualquer coisa que não seja SIM ou NÃO, o programa é e ncerrado e retorna para o menu.
-		}while(p->subd == NULL && p->sube == NULL);
-      	  					
-  getch();
+		}while(p->subd != NULL && p->sube != NULL);
+}
+  
+/************************************************ 
+ * entrada_dados                                *
+ * objetivo: rotina para ler dados              *
+ * entrada : nodo (ponteiro para o novo espaco) *
+ * saída   : nodo com dados                      *
+ ************************************************/
+void entrada_dados( ARVORE* aux )
+{ 
+/*  printf( "\n\n Código: %d", aux->info.codigo ); */  
+    aux->subd = NULL;    // não aponta
+    aux->sube = NULL;    // não aponta
+
 }
 
 /*************************************************
@@ -154,7 +189,7 @@ void imprime_ARVORE( ARVORE* aux ){
     if( aux != NULL ){              // verifica se a raiz é diferente de vazio
         printf( "\n Código.....: %d", aux->info.codigo );
         printf( "\n Altura.....: %d", aux->info.altura );
-        printf( "\n Pergunta.....: %s", aux->info.pergunta );        
+        printf( "\n Fatbal.....: %s", aux->info.nome );        
 		getchar();
         printf( "\n >> sube" );
         imprime_ARVORE( aux->sube ); // recursivo, segue pelo caminho da esquerda
@@ -175,6 +210,82 @@ void cria_ARVORE( ARVORE** r ){
     *r = NULL; // arvore criada, raiz nao aponta
 }
 
+/************************************************* 
+ * insere no Fim                                 *
+ * objetivo: rotina para inserir no fim da ARVORE*
+ * entrada : ARVORE                              *
+ * saída   : ARVORE com mais um registro         *
+ *************************************************/ 
+void insere( ARVORE** r ){ 
+    ARVORE* p;      // ponteiro auxiliar
+    ARVORE* a;      // ponteiro auxiliar para anterior de p
+    int cod, achou; // cod = dado de entrada; achou = informa se código já existe na estrutura
+
+    printf("\n Digite o novo código: ");
+    scanf("%d", &cod);
+    
+    p = *r;                            // posiciona ponteiro auxiliar
+    achou = busca( cod, &a, &p );      // verifica se código a ser inserido já existe na árvore
+    if( !achou ){                      // se não achou o código, insere
+         ARVORE* no = ( ARVORE * ) malloc ( sizeof( ARVORE )); // aloca novo espaco em memória
+
+         if( no != NULL ){             // se conseguiu alocar memória, insere
+                 no->info.codigo = cod;// preenche o novo registro com o código já informado
+                 entrada_dados( no );  // entrada do usuário
+                 if( *r == NULL )      // verifica se a árvore esta vazia
+                     *r = no;          // o registro sera o primeiro, atualiza a raiz
+                 else 
+                      if ( no->info.codigo > a->info.codigo )
+                           a->subd = no; // insere a direita do registro folha
+                      else
+                           a->sube = no; // insere a esquerda do registro folha
+         } // fim if( no != NULL )
+    } // fim if( !achou )
+    else
+        printf( "\n Registro já existe!" );
+}
+
+/************************************************ 
+ * busca                                        *
+ * objetivo: achar nodo                         *
+ * entrada : ARVORE e código a ser excluído     *
+ * saída   : posicao ou NULL (não encontrou)    *
+ ************************************************/ 
+int busca( int cod, ARVORE** a, ARVORE** p ){
+   int achou = FALSE;                // achou = indica se encontrou o código na estrutura
+   
+   *a = NULL;                        // ponteiro auxiliar para o anterior
+   while( ( *p != NULL ) && ( !achou ) )
+           if ( (*p)->info.codigo == cod ) // veririfica se encontrou o código
+                achou = TRUE;        // encontrou a matricula na estrutura
+           else {
+                *a = *p;             // posiciona ponteiro auxiliar no anterior de p
+                if( (*p)->info.codigo > cod )
+                    *p = (*p)->sube; // anda com o ponteiro p para a esquerda, pois o código procurada e menor
+                else
+                    *p = (*p)->subd; // anda com o ponteiro p para a direita, pois o código procurada e maior 
+           } // fim if ((*p)->info.codigo == cod)
+    
+   return achou; 
+}
+
+/***************************************************
+ * busca_recursivo                                 *
+ * objetivo: rotina para buscar registro por código*
+ * entrada : ARVORE e cod                          *
+ * saída   : ARVORE com mais um registro           *
+ ***************************************************/ 
+ARVORE* busca_recursivo( ARVORE *p, int cod ){
+   if( p == NULL || p->info.codigo == cod )
+       return p; 
+   else{
+       if( p->info.codigo > cod )
+            return busca_recursivo( p->sube, cod ); // anda com o ponteiro p para a esquerda, pois o código procurada é menor
+       else
+            return busca_recursivo( p->subd, cod ); // anda com o ponteiro p para a direita, pois o código procurada é maior 
+   } // fim if( *p == NULL || (*p)->info.codigo == cod )
+}
+
 /************************************************
  * gera_dados                                   *
  * objetivo: rotina para ler dados dos registros*
@@ -184,27 +295,28 @@ void cria_ARVORE( ARVORE** r ){
  
  //void SetPessoa(Pessoa *P, int idade, float peso, float altura)
 
-void gera_dados(ARVORE **r)
-{
+void gera_dados(ARVORE **r){
 	
 		char *perguntas[] = {
-		"Você realizou alguma aplicação nos últimos 12 meses?",
-		"Faria investimentos para longo prazo, sem poder resgatar o seu dinheiro nos próximos 5 anos ?",
-		"Investiria seu dinheiro para recuperar o mesmo no mesmo dia ou semana, independente de resultados positivos ou negativos ?",
-		"Você sabe a diferença entre LCI, LCA Ações e Debêntures ?",
-		"Conservador" ,
-		"Moderado",
+		"Você realizou alguma aplicação nos últimos 12 meses?", //pos 1 - cod 41
+		"Faria investimentos para longo prazo, sem poder resgatar o seu dinheiro nos próximos 5 anos ?", //pos 2 - cod 20
+		"Prioriza segurança para que não perca dinheiro ? ", //pos 3 - cod 65
+		"Investiria seu dinheiro para recuperar o mesmo no mesmo dia ou semana, independente de resultados positivos ou negativos ?",//pos 4 - cod 11
+		"Investiria mais de 1000 reais por mês sem liquidez diária/mensal ? " , //pos 5 - cod 30
+        //pos 6 - cod 48
+		"Investiria mais de 1000 reais mensalmente caso tenha renda para isso?", //pos 7 - cod 91 
+		//pos 8 - cod 8
+		"Moderado", 
+		"Conservador", //pos 9  - cod  
 		"Ao Montar sua carteira de investimentos você procuraria um especialista para lhe auxiliar ? ",
 		"Agressivo",
+		"Você sabe a diferença entre LCI, LCA Ações e Debêntures ?", //8
 		"Moderado",
-		"Investiria mais de 1000 reais por mês sem liquidez diária/mensal ? ",
 		"Ja investiu em ações ? ",
 		"Conservador",
 		"Agressivo",
 		"Considerando seus investimentos de 1000 do mês passado,você se depara com uma queda, resgata o seu dinheiro? ",
 		"Moderado",
-		"Conservador",
-		"Prioriza segurança para que não perca dinheiro ? ",
 		"Supondo o caso de que você investisse 1000 em 1 mês. Você teria grandes problemas se investisse e perdesse os 1000 reais ? ",
 		"Em relação ao seu planejamento financeiro, você se considera organizado ?",
 		"Moderado",
@@ -212,7 +324,6 @@ void gera_dados(ARVORE **r)
 		"Se a perda de uma quantia em dinheiro te traria problemas, certamente necessita mais conhecimento, quando falamos em investimento, você acha os assuntos abordados interessantes? ",
 		"Conservador",
 		"Moderado",
-		"Investiria mais de 1000 reais mensalmente caso tenha renda para isso?",
 		"E ao comprar produtos de investimento você considera uma tarefa fácil para conseguir realiza-lá sozinho ?",
 		"Conservador",
 		"Moderado",
@@ -222,43 +333,43 @@ void gera_dados(ARVORE **r)
 		
 		};
 		
-		int vetPosicoes[]= {41,20,65,11,30,48,91,8,17,28,35,46,50,72,99,6,9,15,19,26,29,32,36,45,47,49,52,70,76,98,100};	  
+		int vetPosicoes[]= {41,20,65,11,30,48,91,8,17,28,35,46,50,72,99,6,9,15,19,26,29,32,36,45,47,49,52,70,76,98,100};
+	int   i, x;                                 // variaveis auxiliares para percorrer o vetor
+for(i = 0 ; i < 31 ; i ++) //laço para percorrer o vetor
+{
 
-	int   i; //variavel auxiliar, apenas para dar indice aos vetores e percorrer o laço FOR
-	
-	for(i = 0 ; i < 16 ; i ++) //laço para percorrer o vetor
-	{
+	int cod = i,achou; // cod = dado de entrada; achou = informa se código já existe na estruturas
+		//p = *r;
+				ARVORE *no = ( ARVORE * ) malloc ( sizeof( ARVORE )); // aloca novo espaco em memória
+			     
+				 if(no != NULL)
+				 {
+			     	no->info.codigo = vetPosicoes[i]; //no info codigo recebe o valor contido na posição i do vetor
+			     	strcpy(no->info.nome,perguntas[i]);//copia o valor da string2 para a variavel 1
+			     	
+				     if(*r == NULL) //verifica se a raiz é null, popular o primeiro registro
+				     {		
+					 		*r = no; //raiz recebe a informação contida em no
+					 }		    		
+					else { 				//caso a raiz não esteja vazia, verificar onde o registro deverá ser inserido
+							ARVORE* atual = *r;      // ponteiro auxiliar
+		    				ARVORE* anterior = NULL;     // ponteiro auxiliar para anterior de atual	
+						while(atual != NULL) //se o atual for diferente de NULL
+						{
+							anterior = atual; //o anterior recebe o endereço do atual
+								if(no->info.codigo > atual->info.codigo) //se o codigo que quero inserir é maior que o atual
+									atual = atual->subd; //então deverá ser inserido à direita
+								else
+									atual = atual->sube; //do contrário, deverá ser inserido na subarvore a esquerda
+						}if(no->info.codigo > anterior->info.codigo) //se o codigo que quero inserir é maior que o anterior
+							anterior->subd = no; //insere na subarvore direita
+						else
+							anterior->sube = no; //insere na subarvore esquerda
+					}
+	                     
+			     }
+		}
 		
-					ARVORE *no = ( ARVORE * ) malloc ( sizeof( ARVORE )); // aloca novo espaco em memória
-				     
-					 if(no != NULL)
-					 {
-				     	no->info.codigo = vetPosicoes[i]; //no info codigo recebe o valor contido na posição i do vetor
-				     	strcpy(no->info.pergunta,perguntas[i]);//copia o valor da string2 para a variavel 1
-				     	
-					     if(*r == NULL) //verifica se a raiz é null, popular o primeiro registro
-					     {		
-						 		*r = no; //raiz recebe a informação contida em no
-						 }		    		
-						else { 				//caso a raiz não esteja vazia, verificar onde o registro deverá ser inserido
-							
-								ARVORE* atual = *r;      // ponteiro auxiliar
-			    				ARVORE* anterior = NULL;     // ponteiro auxiliar para anterior de atual	
-							while(atual != NULL) //se o atual for diferente de NULL
-							{
-								anterior = atual; //o anterior recebe o endereço do atual
-									if(no->info.codigo > atual->info.codigo) //se o codigo que quero inserir é maior que o atual
-										atual = atual->subd; //então deverá ser inserido à direita
-									else
-										atual = atual->sube; //do contrário, deverá ser inserido na subarvore a esquerda
-							}if(no->info.codigo > anterior->info.codigo) //se o codigo que quero inserir é maior que o anterior
-								anterior->subd = no; //insere na subarvore direita
-							else
-								anterior->sube = no; //insere na subarvore esquerda
-						}
-		                     
-				     }//fim do IF no != NULL
-			} //fim do laço de repetição
-} // fim da função principal
+	}
 
 
